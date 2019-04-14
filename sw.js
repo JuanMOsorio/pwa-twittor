406a53dce99fc25a64467868fe7c20dd0fa0176d
Juan Martínez Osorio <juan24.martinez05@gmail.com>
2019-04-14 17:40:35 -0500

    Finalizado

--- sw.js
+++ sw.js
@@ -1,67 +1,100 @@
-// Imports
+// imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const IMNUTABLE_CACHE = 'inmutable-v1';
 
-const APP_SELL = [
-  // '/',
-  'index.html',
-  'css/style.css',
-  'img/favicon.ico',
-  'img/avatars/hulk.jpg',
-  'img/avatars/ironman.jpg',
-  'img/avatars/spiderman.jpg',
-  'img/avatars/thor.jpg',
-  'img/avatars/wolverine.jpg',
-  'js/app.js',
-  'js/sw-utils.js'
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    // '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
-const APP_SELL_INMUTABLE = [
-  'https://fonts.googleapis.com/css?family=Quicksand:300,400',
-  'https://fonts.googleapis.com/css?family=Lato:400,300',
-  'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-  'css/animate.css',
-  'js/libs/jquery.js'
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
-// Intalación
-self.addEventListener('install', event => {
 
-  // Creando el cache estatico.
-  const cacheStatic = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SELL));
-  // Creando el cache inmutable.
-  const cacheInmutable = caches.open(IMNUTABLE_CACHE).then(cache => cache.addAll(APP_SELL_INMUTABLE));
 
-  event.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+self.addEventListener('install', e => {
+
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+
 });
 
-// Borrar los caches anteriores.
-self.addEventListener('activate', event => {
-  const d = caches.keys().then(keys => {
-    keys.forEach(key => {
-      if (key !== STATIC_CACHE && key.includes('static')) {
-        return caches.delete(key);
-      }
+
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
+        });
+
     });
-  });
 
-  event.waitUntil(d);
+    e.waitUntil( respuesta );
+
 });
 
 
-self.addEventListener('fetch', event => {
-  const respond = caches.match(event.resquest).then(res => {
-    if (res) {
-      return res;
-    } else {
-      return fetch(event.request).then(newRes => {
-        return updateDynamicCache(DYNAMIC_CACHE, event.request, newRes);
-      });
-    }
-  });
 
-  event.respondWith(respond);
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return updateDynamicCache( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
 });
+
+
